e56a2b3c38d832df1bbf17f762bc6ca90f5b2a3c
Logan Bready <logan.bready@gmail.com>
2021-07-19 13:04:00 -0600

    new

--- project.js
+++ project.js
@@ -2,4 +2,9 @@ function getGreeting(name){
     return "Hi " + name;
 }
 
+function getIt (it){
+    return "get" + it;
+}
+
+module.exports = getIt;
 module.exports = getGreeting;
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 const getGreeting = require('./project');
+const getIt = require('./project')
 
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('can get it', () => {
+  expect(getIt('chick')).toBe('get chick')
+})
